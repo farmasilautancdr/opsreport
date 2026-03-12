c998343a0a8d81d6174be36678b0b5145a8a64ba
farmasilautancdr <farmasilautan.cdr@gmail.com>
2026-03-12 11:45:07 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,23 @@
-const CACHE_NAME = 'ops-portal-v1';
-const assets = ['index.html', 'manifest.json'];
+const CACHE_NAME = 'ops-portal-v3.2';
+const assets = [
+  'index.html',
+  'manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
